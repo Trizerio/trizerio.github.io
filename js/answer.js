f0754ac0c8efa3023631550cd0ab9d240be909e5
Trizerio <31944244+Trizerio@users.noreply.github.com>
2019-03-04 18:26:55 +0300

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -6,7 +6,7 @@ $(function() {
 	}
 	sendQuestion = () => {
 		var text = $('input[type="text"]').val();
-		if(text.toLowerCase().match(/(.*) или (.*)/g)) {
+		if(text.match(/(.*) или (.*)/g)) {
 			var words = text.split(' ИЛИ ' || 'Или' || 'или');
 			words = words.toString().replace('?', '').split(',');
 			var random = ~~(Math.random() * words.length);
