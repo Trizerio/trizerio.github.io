90466ebaaea5b2537dfb0b3b6f276fcb1b026e40
Trizerio <31944244+Trizerio@users.noreply.github.com>
2019-03-04 15:11:22 +0300

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -1,45 +1,32 @@
 $(function() {
+	let answerer = {
+		error: `<div class="animated fadeInUp answer" type="error" style="color: #de6969 !important;">Пожалуйста, введите свой вопрос</div>`,
+		yes: `<div class="animated fadeInUp answer"><label>${$('input[type="text"]').val()} — </label>Да</div>`,
+		no: `<div class="animated fadeInUp answer" style="color: #de6969 !important;"><label>${$('input[type="text"]').val()} — </label> Нет</div>`,
+		count: 0
+	}
+	sendQuestion = () => {
+	  	if($('input[type="text"]').val().length < 1 || !isNaN($('input[type="text"]').val())) {
+	  		$('.answer').html(answerer.error);
+	  	} else {
+	  		let answer = ["yes", "no"];
+	  		let random = ~~(Math.random() * answer.length);
+	  		switch(answer[random]) {
+	  			case 0:
+	  				$('.answer').html(answerer.yes);
+	  			break;
+	  			case 1:
+	  				$('.answer').html(answerer.no);
+	  			break;
+	  		}
+	  	}
+	}
 
-  var answer_count = 0;
-
-  sendQuestion = function() {
-    var answer = ['Да', 'Нет'];
-    var random = Math.floor(Math.random() * answer.length);
-    if($('input[type="text"]').val().length < 1 || !isNaN($('input[type="text"]').val())) {
-      $('.answer').remove();
-      $('#answer-holder').prepend(`<div class="animated fadeInUp answer" type="error" style="color: #de6969 !important;">Пожалуйста, введите свой вопрос</div>`);
-    } else {
-      $('.answer[type="error"]').remove();
-      if(answer_count > 0) {
-        $('.answer').remove();
-        if(answer[random] == 'Да') {
-          $('#answer-holder').prepend(`<div class="animated fadeInUp answer"><label>${$('input[type="text"]').val()} — </label>Да</div>`);
-        } else {
-          $('#answer-holder').prepend(`<div class="animated fadeInUp answer" style="color: #de6969 !important;"><label>${$('input[type="text"]').val()} — </label> Нет</div>`);
-        }
-        answer_count +=1;
-      }
-      if(answer_count < 1) {
-        if(answer[random] == 'Нет') {
-          $('#answer-holder').prepend(`<div class="animated fadeInUp answer" style="color: #de6969 !important;"><label>${$('input[type="text"]').val()} — </label> Нет</div>`);
-        } else {
-          $('#answer-holder').prepend(`<div class="animated fadeInUp answer"><label>${$('input[type="text"]').val()} — </label>Да</div>`);
-        }
-        answer_count += 1;
-      }
-    }
-  };
-
-  $('input[type="text"]').on('keydown', function(e) {
-      if (e.which == 13) {
-          e.preventDefault();
-          sendQuestion();
-          $('input[type="text"]').val("");
-      }
-  });
-
-  $('button.btn').click(function()  {
-    sendQuestion();
-    $('input[type="text"]').val("");
-  });
+	$('input[type="text"]').on('keydown', function(e) {
+		if(e.which == 13) {
+			e.preventDefault();
+			sendQuestion();
+			$('input[type="text"]').val("");
+		}
+	});
 });
