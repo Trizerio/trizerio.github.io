b7bf9841790ed5a2c267ae2a8b000e4d6f7f2ba0
Trizerio <31944244+Trizerio@users.noreply.github.com>
2019-03-04 17:20:33 +0300

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -5,21 +5,29 @@ $(function() {
 		no: `<div class="animated fadeInUp answer" style="color: #de6969 !important;">Нет</div>`
 	}
 	sendQuestion = () => {
+		var text = $('input[type="text"]').val();
+		if(text.match(/(.*) или (.*)[?]/g)) {
+			var words = text.split(' или ');
+			console.log(words);
+			var random = ~~(Math.random() * words.length);
+			console.log(words[random]);
+			$('input[type="text"]').val("");
+			$("#answer-holder").prepend(`<div class="animated fadeInUp answer"><label>${words[0]} или ${words[1]}</label>` + words[random] + "</div>"); return;
+		}
 	  	if($('input[type="text"]').val().length > 1) {
-	  		let answer = [answerer.yes, answerer.no];
+	  		var answer = [answerer.yes, answerer.no];
 	  		var random = ~~(Math.random() * answer.length);
 	  		$("#answer-holder").prepend(`<div class="animated fadeInUp answer"><label>${$('input[type="text"]').val()}</label>` + answer[random] + "</div>");
 	  	} else {
 	  		$('#answer-holder').prepend(answerer.error);
 	  	}
 	}
-	$('input[type="text"]').on('keydown', function(e) {
+	$('input[type="text"]').on('keydown', (e) => {
 		if(e.which == 13) {
 			e.preventDefault();
 			$('.answer[type="error"]').remove();
 			$('.answer').remove();
 			sendQuestion();
-			$('input[type="text"]').val("");
 		}
 	});
 });
